7b37fa0b7fa5fae2c0bb50c87e9a55f8ce5c40c4
wari <nicolaycamacho@gmail.com>
2019-08-12 15:28:53 -0400

    Ravenous Part I finished, Background images path corrected and BusinessList component corrected

--- ravenous/src/components/BusinessList/BusinessList.js
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./BusinessList.css";
 import Business from "../Business/Business";
 
-class BusinessList extends React.Components {
+class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
