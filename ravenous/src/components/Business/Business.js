0ce0fc8c6a32e7969591419bbffe9ccb94f595c3
wari <nicolaycamacho@gmail.com>
2019-08-11 23:09:07 -0400

    Business component content added

--- ravenous/src/components/Business/Business.js
+++ ravenous/src/components/Business/Business.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const business = {
   imageSrc:
     "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
@@ -10,3 +12,33 @@ const business = {
   rating: 4.5,
   reviewCount: 90
 };
+
+class Business extends React.Component {
+  render() {
+    return (
+      <div className="Business">
+        <div className="image-container">
+          <img
+            src="https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg"
+            alt=""
+          />
+        </div>
+        <h2>{Business.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{Business.address}</p>
+            <p>{Business.city}</p>
+            <p>
+              {Business.state} {Business.zipCode}
+            </p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{Business.category}</h3>
+            <h3 className="rating">{Business.rating} stars</h3>
+            <p>{Business.reviewCount}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
