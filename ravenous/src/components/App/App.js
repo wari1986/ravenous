b42224d98c8274b0f3ef6f7bad3a109fcb69f4a2
wari <nicolaycamacho@gmail.com>
2019-08-13 01:48:45 -0400

    this. removed on value on line 27 App.js

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -18,14 +18,16 @@ const business = {
 
 const businesses = [business, business, business, business, business, business];
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />;
-      <BusinessList business={this.businesses} />;
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
