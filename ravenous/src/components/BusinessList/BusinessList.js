90ac7a1db0dd46c95c50e0ac138deb9a6afa5d8b
wari <nicolaycamacho@gmail.com>
2019-08-13 01:09:15 -0400

    businesses prop added and .map method called on BusinessList div

--- ravenous/src/components/BusinessList/BusinessList.js
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -4,15 +4,7 @@ import Business from "../Business/Business";
 
 class BusinessList extends React.Component {
   render() {
-    return (
-      <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-      </div>
-    );
+    return <div className="BusinessList">{this.props.businesses.map()}</div>;
   }
 }
 
